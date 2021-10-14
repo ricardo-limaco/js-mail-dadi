58eec3d93647f368d4448a750f9f4f93cf506ff1
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-14 10:21:15 +0200

    JS Completed

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -5,19 +5,7 @@ let cpu = Math.floor(Math.random()*6) + 1;
 console.log("Numero dadi cpu:",cpu);
 
 if(utente > cpu){
-    alert("Hai vinto")
+    alert("Hai vinto");
 } else {
-    alert("Hai perso")
-}
-
-
-
-
-
-
-
-
-/*
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-*/
+    alert("Hai perso");
+} 
